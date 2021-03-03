7239cb3fda242b4fa016f8966731af685f15d774
Danst-bjtu <77576766+Danst-bjtu@users.noreply.github.com>
2021-03-03 14:05:19 +0800

    质检界面

--- flask_label_neo4j/main.py
+++ flask_label_neo4j/main.py
@@ -36,8 +36,18 @@ def just_label(item_id):
     zhanbi = round(int(islabel_count[0]['count(q)'])*100/int(sen_count[0]['count(q)']), 2)
     entity_type = graph.run("MATCH (n:label_items)-[r:`包含`]-(m:`实体类别`) where ID(n)=" + item_id + " return m").data()
     relation_type = graph.run("MATCH (n:label_items)-[r:`包含`]-(m:`关系类别`) where ID(n)=" + item_id + " return m").data()
-    fenju = graph.run("match(n:label_items)-[r:`文件路径`]->(m),(m)-[s:分句]->(q) where ID(n)="+item_id+" return ID(q),q.content,q.islabel order by ID(q)").data()
-    return render_template('just_label.html', item_id=item_id, sen_count=sen_count, islabel_count=islabel_count, zhanbi=zhanbi,fenju=str(fenju),entity_type = entity_type, relation_type = relation_type)
+    return render_template('just_label.html', item_id=item_id, sen_count=sen_count, islabel_count=islabel_count, zhanbi=zhanbi,entity_type = entity_type, relation_type = relation_type)
+
+
+@app.route('/label_inspection/<item_id>')
+def label_inspection(item_id):
+    sen_count = graph.run("match(n:label_items)-[r:`文件路径`]->(m),(m)-[s:分句]->(q) where ID(n)="+item_id+" return count(q)").data()
+    islabel_count = graph.run("match(n:label_items)-[r:`文件路径`]->(m),(m)-[s:分句]->(q) where ID(n)="+item_id+" and q.islabel='yes' return count(q)").data()
+    zhanbi = round(int(islabel_count[0]['count(q)'])*100/int(sen_count[0]['count(q)']), 2)
+    entity_type = graph.run("MATCH (n:label_items)-[r:`包含`]-(m:`实体类别`) where ID(n)=" + item_id + " return m").data()
+    relation_type = graph.run("MATCH (n:label_items)-[r:`包含`]-(m:`关系类别`) where ID(n)=" + item_id + " return m").data()
+    return render_template('label_inspection.html', item_id=item_id, sen_count=sen_count, islabel_count=islabel_count, zhanbi=zhanbi,entity_type = entity_type, relation_type = relation_type)
+
 
 if __name__ == '__main__':
     app.config['JSON_AS_ASCII'] = False
